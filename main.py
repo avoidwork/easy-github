ce694733249042980fe4a52440e017080b99b2c9
Jason Mulligan <jason.mulligan@avoidwork.com>
2025-05-18 21:42:52 -0400

    Making a minor change

--- main.py
+++ main.py
@@ -58,8 +58,7 @@ class Tools:
         repo_list.sort(key=lambda x: x["stars"], reverse=True)
         if len(repo_list) > top:
             repo_list = repo_list[:top]
-        result = f"""
-Show a list of the top {len(repo_list)} results:
+        result = f"""Show a list of the top {len(repo_list)} results:
 
 {json.dumps(repo_list)}
 """
